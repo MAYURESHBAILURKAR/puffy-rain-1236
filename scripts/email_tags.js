dc5d6f5f399c57d2d9d64be8eb94778d640b1806
MAYURESHBAILURKAR <mayureshbailurkar@gmail.com>
2022-07-22 23:58:42 +0530

    pagesAdded

---
+++ scripts/email_tags.js
@@ -0,0 +1,9 @@
+import navbar from "../components/navbar.js";
+
+document.querySelector("#navbar1").innerHTML = navbar();
+
+
+
+import footer from "../components/footer.js";
+
+document.querySelector("#footer1").innerHTML = footer();
