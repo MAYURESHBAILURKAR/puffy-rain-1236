d913e941c3ee09457a733fc08fea7899a1cfe5e1
chinmaydonkar <chinmaydonkar@gmail.com>
2022-07-23 14:58:32 +0530

    updated links

--- scripts/email_tags.js
+++
@@ -1,9 +0,0 @@
-import navbar from "../components/navbar.js";
-
-document.querySelector("#navbar1").innerHTML = navbar();
-
-
-
-import footer from "../components/footer.js";
-
-document.querySelector("#footer1").innerHTML = footer();
